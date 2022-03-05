d54a7add838bf1e981387a6556ae513d4606de35
nsotosca <nahuelsotosca@gmail.com>
2022-03-05 19:48:19 -0300

    add eslint configurations

---
+++ src/App.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  // eslint-disable-next-line react/react-in-jsx-scope
+  render(<App />);
+  const linkElement = screen.getByText(/Hola Mundo!/i);
+  expect(linkElement).toBeInTheDocument();
+});
