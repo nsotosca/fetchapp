a3246318a96411a76316536a091bf7b84f17de11
nsotosca <nahuelsotosca@gmail.com>
2022-03-05 20:18:53 -0300

    add eslint configurations

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -7,5 +7,4 @@ test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/Hola Mundo!/i);
   expect(linkElement).toBeInTheDocument();
-  expect(linkElement).toBeInTheDocument();
 });
