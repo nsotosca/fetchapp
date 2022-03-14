eada36e9f19761351da87f98c0ba4740291b6cbd
nsotosca <nahuelsotosca@gmail.com>
2022-03-14 19:29:54 -0300

    change validation in post item

--- src/components/PostItem/PostItem.tsx
+++ src/components/PostItem/PostItem.tsx
@@ -16,15 +16,17 @@ const PostItem = ( props: PostItemProps ) :JSX.Element => {
   const { t } = useTranslation();
   const isUrl = value.toString().split( '/', 6 ).length > 5;
   const url = isUrl ? ( value.toString().split( '/', 6 ).slice( 4, 6 ).join( '/' ) + '/' ) : '';
-  const { data, loading } = useApi( url, ['name'], false );
+  const { data } = useApi( url, ['name'], false );
 
   const Value = (): JSX.Element => {
-    if( loading && isUrl )
-      return <>Cargando...</>;
-    if( data.length > 0 )
+    if( !url ) {
+      return t( value );
+    }
+    if( data.length > 0 ) {
       return  t( data[0].name );
+    }
 
-    return t( value );
+    return <>Cargando...</>;
   };
 
 
